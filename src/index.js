5cb70a5f04c6a2c6d50bef35e6fcc9bac3098882
thisisyoungjun <122899@naver.com>
2022-08-23 16:59:05 +0900

    22-08-23

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-$("#nav").on('click',function (){
+$(".ContentNav").on('click',function (){
     console.log("asd")
-    if ($(".ContentsBox").css("top")=="1000px"){
-            $(".ContentsBox").stop().animate({top:550},'slow')
+    if ($(".Contents_Nav_box").css("top")=="1000px"){
+            $(".Contents_Nav_box").stop().animate({top:550},'slow')
     }else {
-        $(".ContentsBox").stop().animate({top:1000},)
+        $(".Contents_Nav_box").stop().animate({top:1000},)
 
     }
 });
+
+$("#content_title_logo").on('click',function (){
+    location.href='../index.html';
+    console.log("asd")
+})
+
+$(".content-box>.col").hover(function (){
+    $(this).css("background-color","white")
+    $(this).find(".content_info").css("display","block")
+
+},function (){
+    $(this).css("background-color","black")
+    $(this).find(".content_info").css("display","none")
+})
+
 
